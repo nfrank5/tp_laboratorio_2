f0f6676418f7d439ac98b0a5eb7cf67bffa636d4
nfrank5 <nfrankenfeld@gmail.com>
2016-09-11 01:17:25 -0300

    documentacionCalculadora

--- Nicolas.Frankenfeld/TP_1/Calculadora.cs
+++ Nicolas.Frankenfeld/TP_1/Calculadora.cs
@@ -8,6 +8,13 @@ namespace TP_1
 {
     class Calculadora
     {
+        /// <summary>
+        /// recibe dos numeros y un operador que realizan la operacion matematica solicitada
+        /// </summary>
+        /// <param name="numero1">objeto de la clase Numero</param>
+        /// <param name="numero2">objeto de la clase Numero</param>
+        /// <param name="operador">string con el operador a ser utilizado</param>
+        /// <returns>retorna el resultado en tipo de dato double</returns>
         public static double operar(Numero numero1, Numero numero2, string operador)
         {
             double retorno =0;
@@ -37,7 +44,11 @@ namespace TP_1
             return retorno;
         }
 
-
+        /// <summary>
+        /// valida el operador, si no es uno de los esperados, retorna "+"
+        /// </summary>
+        /// <param name="operador">recibe un string</param>
+        /// <returns>retorna el operadro en formato string</returns>
         public static string validarOperador(string operador)
         {
             string retorno = "+";
